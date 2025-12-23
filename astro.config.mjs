880f1f85fe54fc31d8388b152d48eac1a858751b
leswell <simofilevente+git@gmail.com>
2025-12-23 22:29:21 +0200

    chore: dev tools setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        allowedHosts: [
+            "0rbt1of2xor9.share.zrok.io"
+        ]
+    }
+});
--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,30 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  languages = {
+    javascript = {
+      enable = true;
+      package = pkgs.nodejs_22;
+      pnpm.enable = true;
+    };
+  };
 
-  
+  # https://devenv.sh/packages/
+  packages = [ 
+    pkgs.git
+    pkgs.zrok
+    pkgs.qrencode
+  ];
+
+  scripts = 
+  let 
+    port = "2002";
+    reserved-name = "0rbt1of2xor9";
+  in
+  {
+    tunnel.exec = "zrok share reserved ${reserved-name}";
+    qr.exec = "qrencode -t ANSIUTF8 'https://${reserved-name}.share.zrok.io'";
+    server.exec = "pnpm dev --port ${port} --host";
+  };
+
 }
