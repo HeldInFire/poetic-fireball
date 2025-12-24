b63bc70898e65ebf9cf2971ee11244d0cacbf957
leswell <simofilevente+git@gmail.com>
2025-12-25 01:56:17 +0200

    chore(zrok): updated reserve

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,7 @@
 { pkgs, lib, config, inputs, ... }:
+let 
+  
+in
 
 {
   languages = {
@@ -19,7 +22,7 @@
   scripts = 
   let 
     port = "2002";
-    reserved-name = "0rbt1of2xor9";
+    reserved-name = "j628afbr91lt";
   in
   {
     tunnel.exec = "zrok share reserved ${reserved-name}";
