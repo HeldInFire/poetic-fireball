745fee2347b2d3c5f4a452da40cfaaa77a795b90
leswell <simofilevente+git@gmail.com>
2025-12-26 18:47:40 +0200

    feat(design): first poem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        allowedHosts: [
-            "0rbt1of2xor9.share.zrok.io"
-        ]
-    }
+  server: {
+      allowedHosts: [
+          "0rbt1of2xor9.share.zrok.io"
+      ]
+  },
+
+  integrations: [mdx()]
 });
