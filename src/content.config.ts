4f913641982beb2b4fe775d574791d69ddcf6e33
leswell <simofilevente+git@gmail.com>
2026-01-06 15:30:36 +0200

    refactor(content-collections): the_lost_five_hundred collection introduced

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Import Zod
+import { z } from 'astro/zod';
+
+const poem_schema = z.object({
+    title: z.string(),
+    written_at: z.date(),
+})
+
+// 4. Define your collection(s)
+const the_lost_five_hundred = defineCollection({ 
+    loader: glob({ pattern: "*.md", base: "./src/era/the-lost-five-hundred" }),
+    schema: poem_schema
+});
+
+const within_awareness = defineCollection({ 
+    loader: glob({ pattern: "**/*.md", base: "./src/era/within-awareness" }),
+    schema: poem_schema
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { the_lost_five_hundred, within_awareness };
