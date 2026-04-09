640f1469abd63ac9b63d6e704d369bdcaa1c3f46
leswell <simofilevente+git@gmail.com>
2026-04-09 13:44:12 +0300

    chore(chakra-ui): configure chakra-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html suppressHydrationWarning lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+
     </html>
   );
 }
---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Provider } from "@/components/ui/provider"
+
+
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <Provider>
+            {children}
+        </Provider>
+    )
+}
